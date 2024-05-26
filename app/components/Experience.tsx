233a2a5e1ecc1762fd04906de75a93f8a73fe2c7
Aidan Ching <achin053@ucr.edu>
2024-05-26 04:21:35 -0700

    Changes

--- app/components/Experience.tsx
+++ app/components/Experience.tsx
@@ -1,6 +1,5 @@
 import { Fira_Code } from "next/font/google";
 const fira = Fira_Code({ subsets: ["latin"] });
-import Image from "next/image";
 import { Separator } from "@/components/ui/separator";
 
 import {
@@ -13,26 +12,6 @@ import {
 } from "@/components/ui/card";
 
 import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
-
-export interface Artwork {
-  artist: string;
-  art: string;
-}
-
-export const works: Artwork[] = [
-  {
-    artist: "Ornella Binni",
-    art: "https://images.unsplash.com/photo-1465869185982-5a1a7522cbcb?auto=format&fit=crop&w=300&q=80",
-  },
-  {
-    artist: "Tom Byrom",
-    art: "https://images.unsplash.com/photo-1548516173-3cabfa4607e9?auto=format&fit=crop&w=300&q=80",
-  },
-  {
-    artist: "Vladimir Malyavko",
-    art: "https://images.unsplash.com/photo-1494337480532-3725c85fd2ab?auto=format&fit=crop&w=300&q=80",
-  },
-];
 
 export interface ExperienceEntry {
   company: string;
