653305ace6afd438367355fd69260fc6545552cb
Aidan Ching <achin053@ucr.edu>
2024-05-26 17:43:37 -0700

    Added contact links

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -2,6 +2,8 @@
 
 import { HeroHighlight, Highlight } from "@/components/ui/hero-highlight";
 import { motion } from "framer-motion";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Hero() {
   return (
@@ -21,8 +23,27 @@ export default function Hero() {
         }}
         className="text-2xl px-4 md:text-4xl lg:text-5xl font-bold text-neutral-700 dark:text-white max-w-4xl leading-relaxed lg:leading-snug text-center mx-auto "
       >
-        {`Hi, I'm Aidan. I am a frontend`} <Highlight>Software Engineer</Highlight> {`located in Southern California.`}
+        {`Hi, I'm Aidan. I am a frontend`}{" "}
+        <Highlight>Software Engineer</Highlight>{" "}
+        {`located in Southern California.`}
       </motion.h1>
+      <div className="w-full relative flex justify-center gap-10 md:justify-around md:gap-0 top-32 md:top-20">
+        <Button title="LinkedIn" link="https://www.linkedin.com/in/aidan-ching-ab40371b9/" iconSrc="/icons/linkedin-icon.svg"/>
+        <Button title="GitHub" link="https://github.com/aidan-ching" iconSrc="/icons/github-icon.svg"/>
+        <Button title="Resume" link="https://drive.google.com/file/d/1oFU2cDfiMNM1r2E__ZWDebhNaxN6pmAG/view?usp=sharing" iconSrc="/icons/document-text.svg"/>
+      </div>
     </HeroHighlight>
+  );
+}
+
+function Button({ title, link, iconSrc }: { title: string, link: string, iconSrc: string }) {
+  return (
+    <a href={link} target="_blank">
+      <button className="border px-6 py-3 font-medium rounded-full bg-slate-300 text-black flex flex-row items-center gap-2">
+        <Image src={iconSrc} width={20} height={20} alt={`${title} icon`} />
+        
+        <div className="hidden sm:block">{title}</div>
+      </button>
+    </a>
   );
 }
